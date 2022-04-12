c8f8c5be97c07d5e2ca8e56260d24e91ecbbb814
kir03 <pehenko.k@icloud.com>
2022-04-12 15:04:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Light // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        enum status
+        {
+            выключен,
+            включен
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            status start = (status)rnd.Next(2);
+            Console.WriteLine($"Сейчас свет {start}");
+            Console.WriteLine("Исправить?");
+            Console.WriteLine("0 - нет");
+            Console.WriteLine("1 - да");
+            int.TryParse(Console.ReadLine(), out int answer);
+            if (answer == 1)
+            {
+                if (start - 1 == 0)
+                    Console.WriteLine($"Понял. Сейчас свет {start - 1}");
+                else
+                    Console.WriteLine($"Понял. Сейчас свет {start + 1}");
+            }
+            else
+                Console.WriteLine("Вы решили ничего не трогать");
+
+        }
+    }
+}
